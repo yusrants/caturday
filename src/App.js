76e2385a504dcb18f6f94c12be7fac56c35ef372
yusrants <yusrants@users.noreply.github.com>
2022-06-16 19:12:32 +0500

    Add cats info

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
+import {useState} from 'react';
+import axios from 'axios';
+
 import logo from './logo.gif';
 import './App.css';
 import Cat from './Cat';
 
 function App() {
+  const [show, setShow] = useState(false);
+  let [cats, setCats] = useState([]);
+
+  const config = {
+    headers:{
+     'x-api-key': '8707c32c-3451-483b-a8e2-7323c80e044e'
+    }
+  };
+
+  const url = 'https://api.thecatapi.com/v1/breeds'
+
+  let showCats = () => {
+    setShow(!show);
+
+    axios.get(url, config).then(res=>
+      {
+        setCats(res.data);
+        let catsData = JSON.stringify(cats);
+        document.cookie= "cats=" + catsData;
+      });
+     
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      { !show && <>
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
          your lucky day... it's a
         <span className="heading"> Caturday! </span> 
-        </p>
-        <button> Find out cat breeds </button>
-
-      </header>
+        </p> 
+        </>
+        }
+        <button className="main-button" onClick={showCats}> { !show ? "Find out all about cats" : "Hide all cat breeds"} </button>
+        {show && <Cat cats={cats}/>}
     </div>
   );
 }
--- src/Cat.js
+++ src/Cat.js
@@ -1,8 +1,33 @@
 
-function Cat() {
+import ReactCountryFlag from "react-country-flag"
+
+function Cat({cats}) {
   return (
-    <div className="CatMain">
-        
+    <div className="catMain">
+        {cats.map(cat => {
+          return (
+            <div className="card">
+              <img className="cat-image" src={cat.image ? cat.image.url : ""} />
+              <h5>Hi, I'm {cat.name}</h5>
+              <p className="desc">
+                I'm from 
+                <ReactCountryFlag
+                countryCode={cat.country_code}
+                svg
+                style={{
+                    fontSize: '2em',
+                    lineHeight: '2em',
+                }}
+                />
+              </p>
+              <p className="desc"> My energy level is {cat.energy_level} </p>
+              <p className="desc"> I'm {cat.dog_friendly >= 5 ? "VERYY" : "a little"} dog friendly too! </p>
+              <p className="desc">
+              <span className="heading"> About Me:   </span> 
+                {cat.description}</p>
+            </div>
+          )
+        })}
     </div>
   );
 }
